9bdd6a2386320f610b3049514a6fa713b6610c52
Peter Hemmen <peter.hemmen@bekk.no>
2015-09-13 22:30:33 +0200

    Stop showing CarId to user

--- src/fsharpFTW/Scripts/main.js
+++ src/fsharpFTW/Scripts/main.js
@@ -4,7 +4,7 @@ $(function() {
     $.getJSON(uri)
         .done(function(data) {
             $.each(data, function(key, item) {
-                $('<tr><td>' + item.id + '</td><td>' + item.make + '</td><td>' + item.model + '</td> <td> <button onClick="deleteCar(' + item.id + ')">Delete ' + item.make + ' ' + item.model + '</button></td> </tr>')
+                $('<tr><td>' + (key+1) + '</td><td>' + item.make + '</td><td>' + item.model + '</td> <td> <button onClick="deleteCar(' + item.id + ')">Delete ' + item.make + ' ' + item.model + '</button></td> </tr>')
                     .appendTo($('#cars tbody'));
             });
         });
