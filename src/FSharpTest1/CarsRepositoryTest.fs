080bb2d75c9f698ecc7a5a7765208287a38405db
Peter Hemmen <peter.hemmen@bekk.no>
2015-09-20 23:51:24 +0200

    Add test project
    Not working well as of now, but the skeleton is there. Need to decouple
    the database from the Repository in order to test it properly.

---
+++ src/FSharpTest1/CarsRepositoryTest.fs
@@ -0,0 +1,36 @@
+﻿module ``Given a Cars repository``
+
+open FsUnit
+open NUnit.Framework
+open fsharpFTW.Controllers
+open fsharpFTW.Models
+
+//open NUnitRunner
+
+let repo = new CarsRepository();
+
+
+//TODO: Decouple the CarsRepository to allow for testing of the functions
+
+[<Test>]
+let ``Get all should return three cars``() =
+    let result = repo.GetAll()
+    result |> Seq.length |> should equal 6
+
+
+//[<Test>]
+//let ``FsUnit test 1``() =
+//    [1; 2; 3] |> should equal [1; 2; 3]
+//
+//[<Test>]
+//let ``FsUnit test 2``() =
+//    1 |> should not' (equal 1)
+//
+//[<Test>]
+//let ``FsUnit test 3``() =
+//    10.1 |> should (equalWithin 0.1) 10.22
+//
+//[<Test>]
+//let ``FsUnit test 4 (should throw exception)``() =
+//    (fun () -> 1 + 2 |> ignore) |> should throw typeof<System.Exception>
+
