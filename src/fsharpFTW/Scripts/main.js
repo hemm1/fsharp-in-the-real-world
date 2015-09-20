86333869a0d89027977be53e9cad00d047f1aa86
Peter Hemmen <peter.hemmen@bekk.no>
2015-09-20 13:39:15 +0200

    Remove car make and model from delete button

--- src/fsharpFTW/Scripts/main.js
+++ src/fsharpFTW/Scripts/main.js
@@ -4,7 +4,7 @@ $(function() {
     $.getJSON(uri)
         .done(function(data) {
             $.each(data, function(key, item) {
-                $('<tr><td>' + (key+1) + '</td><td>' + item.make + '</td><td>' + item.model + '</td> <td> <button onClick="deleteCar(' + item.id + ')">Delete ' + item.make + ' ' + item.model + '</button></td> </tr>')
+                $('<tr><td>' + (key + 1) + '</td><td>' + item.make + '</td><td>' + item.model + '</td> <td> <button onClick="deleteCar(' + item.id + ')">Delete car</button></td> </tr>')
                     .appendTo($('#cars tbody'));
             });
         });
