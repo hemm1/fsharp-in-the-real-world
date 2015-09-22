bcc365dcee7eb7d7273c63cbd2d8ece77571dad9
Peter Hemmen <peter.hemmen@bekk.no>
2015-09-22 14:11:40 +0200

    Create Part3 which is just a copy of Part2

---
+++ Part3/fsharpFTW/Controllers/CurrencyController.fs
@@ -0,0 +1,16 @@
+﻿namespace fsharpFTW.Data
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Net.Http
+open System.Web.Http
+open fsharpFTW.Models
+
+type CurrencyController(repo : CurrencyRepository) =
+    inherit ApiController()
+
+    new() = new CurrencyController(CurrencyRepository())
+
+    [<HttpGet>]
+    [<Route("api/currency")>]
+    member x.Convert (fromCurrency:string) (toCurrency:string) (amount:int) = repo.Convert fromCurrency toCurrency amount
