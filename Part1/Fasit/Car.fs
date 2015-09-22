8df9d6231b21097b128c3073d9606b3c159ad077
severin <severin@sverdvik.no>
2015-09-22 13:09:37 +0200

    Part1 fasit

---
+++ Part1/Fasit/Car.fs
@@ -0,0 +1,11 @@
+﻿module Car
+
+type Car = {
+        Make : string
+        Model : string
+        Color : string
+    }   
+
+
+let Repaint car color = 
+    {Make = car.Make; Model = car.Model; Color = color}
---
+++ Part1/Fasit/Program.fs
@@ -0,0 +1,23 @@
+﻿module Main
+open Car
+open System
+
+let createCar make model color = {Make = make; Model = model; Color = color}
+
+let printCar car = printf "Make: %s  Model:%s  Color: %s \n" car.Make car.Model car.Color
+
+[<EntryPoint>]
+let main argv = 
+
+    let car = { Make = "Volvo"; Model = "V70"; Color = "Red"}
+    printCar car
+
+    Car.Repaint car "Blue" |> printCar
+        
+    [ createCar "Mazda" "MX2" "Red"; createCar "Toyota" "Corolla" "Red"; createCar "Volvo" "240" "Yellow"] 
+        |> List.map (fun c -> Car.Repaint c "Blue")
+        |> List.iter (printCar)
+    
+    let s = Console.ReadLine()    
+    0
+
