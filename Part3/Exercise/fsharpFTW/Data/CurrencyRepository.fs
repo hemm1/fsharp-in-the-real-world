e10cc4296c7b06a9e7990252eabf0ef92a9f5418
fiskeboss <severisv@gmail.com>
2015-09-26 10:56:23 +0200

    remove solution from exercise 3a

--- Part3/Exercise/fsharpFTW/Data/CurrencyRepository.fs
+++ Part3/Exercise/fsharpFTW/Data/CurrencyRepository.fs
@@ -4,12 +4,9 @@ open System.Data.Linq
 open Microsoft.FSharp.Data.TypeProviders
 open FSharp.Data
 
-type CurrencyProvider = JsonProvider<"http://api.fixer.io/latest?base=EUR">
 
 type CurrencyRepository() =
                         
     member x.ConvertFromEuroToNok amount =
-        let sample = CurrencyProvider.GetSample()
-        amount * sample.Rates.Nok;
-    
+        0    
             
