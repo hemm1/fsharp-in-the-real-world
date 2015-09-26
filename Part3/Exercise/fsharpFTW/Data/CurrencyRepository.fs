54abf1e240bff971683893ef8029b5fccc4f3e59
fiskeboss <severisv@gmail.com>
2015-09-26 10:50:22 +0200

    Revert "remove solution for 3a"
    This reverts commit fca6f312b9a68fa0ccc1fafa50b433c987ebe8f0.

--- Part3/Exercise/fsharpFTW/Data/CurrencyRepository.fs
+++ Part3/Exercise/fsharpFTW/Data/CurrencyRepository.fs
@@ -4,9 +4,12 @@ open System.Data.Linq
 open Microsoft.FSharp.Data.TypeProviders
 open FSharp.Data
 
+type CurrencyProvider = JsonProvider<"http://api.fixer.io/latest?base=EUR">
 
 type CurrencyRepository() =
                         
     member x.ConvertFromEuroToNok amount =
-        0   
+        let sample = CurrencyProvider.GetSample()
+        amount * sample.Rates.Nok;
+    
             
